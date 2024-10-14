85104d23380da56c4f1f757cae04091cbbf61139
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2024-10-14 17:17:11 +0530

    Update client.py

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -228,7 +228,7 @@ class Client(Methods):
     # Interval of seconds in which the updates watchdog will kick in
     UPDATES_WATCHDOG_INTERVAL = 15 * 60
 
-    MAX_CONCURRENT_TRANSMISSIONS = 1
+    MAX_CONCURRENT_TRANSMISSIONS = 1000
     MAX_MESSAGE_CACHE_SIZE = 10000
 
     mimetypes = MimeTypes()
